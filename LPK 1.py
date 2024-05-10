92ea96a1c76c37975692701b6b73028ba7ffd182
Aapalahhh <fadillachairunnisa03@gmail.com>
2024-05-10 18:06:41 +0700

    Add files via upload

---
+++ LPK 1.py
@@ -0,0 +1,5 @@
+if test == "Home":
+    st.title(':blue[Aplikasi penentu pH Larutan]')
+    st.markdown('''Hai users, selamat datang di web kami.😊''')
+    st.markdown('''Aplikasi ini dapat digunakan untuk menentukan nilai pH dari suatu larutan atau sampel dengan menginput konsentrasi (Molaritas) .''')
+        
